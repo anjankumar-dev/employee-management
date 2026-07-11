dfbdccf4a44fa9fc4f99a1f7668376c7560930ee
Anjan <anjankumar.gogu@gmail.com>
2026-07-11 18:08:57 +0530

    Integrated Swagger Open API for checking  documentation

--- src/main/java/com/anjan/employee_management/dto/AppConfig.java
+++ src/main/java/com/anjan/employee_management/dto/AppConfig.java
@@ -1,5 +1,7 @@
 package com.anjan.employee_management.dto;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,14 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee Management System")
+                        .version("1.0")
+                        .description("API document for managing employee data"));
     }
 }
